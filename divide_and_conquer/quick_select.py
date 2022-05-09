02f4b9ca33ad9bb92a03cafb07ff57b2bd25f9a4
PointBreaker <475669397@qq.com>
2022-05-09 08:54:21 +0800

    remove wrong doctest of quick_select

--- divide_and_conquer/quick_select.py
+++ divide_and_conquer/quick_select.py
@@ -1,13 +1,4 @@
 def quick_select(nums, k):
-    """
-    return the k-th smallest number in nums
-    >>> v = [1, -1, -1, 1]
-    >>> matrix_vector(v, 4)
-    [0, 0, 0, 4]
-    >>> v = [1, 2, 3, 4]
-    >>> matrix_vector(v, 4)
-    [10, -2, -4, 0]
-    """
     assert k > 0, "k is 1-index num"
     pivot = nums[0]
     small, v, large = [], [], []
