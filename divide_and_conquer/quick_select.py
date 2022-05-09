2aae76ac098953c29418bfd4baba67dc88182ed4
PointBreaker <475669397@qq.com>
2022-05-09 09:02:09 +0800

    add doctest for quick_select

--- divide_and_conquer/quick_select.py
+++ divide_and_conquer/quick_select.py
@@ -1,4 +1,20 @@
 def quick_select(nums, k):
+    """
+    >>> nums = [3, 5, 9, 2, 7, 0]
+    >>> quick_select(nums, 1)
+    0
+    >>> quick_select(nums, 5)
+    7
+    >>> quick_select(nums, 3)
+    3
+    >>> nums = [15, 1, 13, 4, 3, 14, 2, 10, 9, 7, 8, 5, 6, 0, 12, 11]
+    >>> quick_select(nums, 2)
+    1
+    >>> quick_select(nums, 7)
+    6
+    >>> quick_select(nums, 16)
+    15
+    """
     assert k > 0, "k is 1-index num"
     pivot = nums[0]
     small, v, large = [], [], []
